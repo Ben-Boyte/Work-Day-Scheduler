75451fb344a12b0af0865280eb07a46996e520d3
Ben-Boyte <zissette1@gmail.com>
2022-05-16 11:32:53 -0500

    added date blocks and editing sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-var time = ["9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm"]
 var container = $(".container");
 var date = $("#currentDay");
 var currentTime = moment().hour()
