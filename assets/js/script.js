e0ea16cc2d349c38402eb7ba5989b31dfa213231
Ben-Boyte <zissette1@gmail.com>
2022-05-16 14:41:18 -0500

    added second array of non-military time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var container = $(".container");
 var time = ["9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"]
+var regularTime = ["9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm"]
 var date = $("#currentDay");
 var currentTime = moment().hour()
 date.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
@@ -25,7 +26,7 @@ function scheduler() {
 
     var label = $("<label>");
             label.attr("class", "col-3 col-sm-1 time-block hour");
-            label.text(time[i]);
+            label.text(regularTime[i]);
             row.append(label);
         
     var textArea = $("<textarea>");
