ba04397462e4dc766eed3ca8620bd786c843bb8e
Ben-Boyte <zissette1@gmail.com>
2022-05-15 20:54:41 -0500

    fixed time on display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
 var time = ["9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm"]
-date.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 var container = $(".container");
 var date = $("#currentDay");
 var currentTime = moment().hour()
+date.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 container.append(date);
 
 function scheduler() {
-    for (i = 0; i < time.length; i++)
-
+    for (i = 0; i < time.length; i++) {
+    let timeInteger = parseInt(time[i])
+    }
+    
+    
 }
 
 window.onload = scheduler();
