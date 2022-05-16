53a523b2c719e6b107155367f785b0d75b9680a2
Ben-Boyte <zissette1@gmail.com>
2022-05-16 12:24:14 -0500

    restored scheduler function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,37 @@ var date = $("#currentDay");
 var currentTime = moment().hour()
 date.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
+function scheduler() {
+    for (i = 0; i < time.length; i++) {
+    let timeInteger = parseInt(time[i])
+
+        if (timeInteger < currentTime) {
+            var moment = "past"
+        }
+       else if (timeInteger > currentTime) {
+            var moment = "future"
+        }
+       else if (timeInteger == currentTime) {
+            var moment = "present"
+        }
+    }
+    
+}
 
 function store() {
     localStorage.setItem($(this).attr("id"), $(this).prev().val());
     $(this).prev().transfer({
-        to: $( $(this) ),
-        duration: 5000
+        to: $($(this)),
     })
 }
 
-$(".saveBtn").on("click", store)
+// $(document).ready(function() {
+//     $(".saveBtn").on(".click", function() {
+//         var text = $(this).siblings(".description").val();
+//         var time = $(this).parent().attr("id");
+//         localStorage.setItem(text, time);
+//     })
+// })
+
+window.onload = scheduler();
+$(".saveBtn").on("click", save)
