4a2614f015f752fc358babb1a0118ab588f6ebd0
Ben-Boyte <zissette1@gmail.com>
2022-05-16 14:44:34 -0500

    updated current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var time = ["9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00
 var regularTime = ["9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm"]
 var date = $("#currentDay");
 var currentTime = moment().hour()
-date.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+date.text(moment().format('MMMM Do YYYY, h:mm a'));
 
 function scheduler() {
     for (i = 0; i < time.length; i++) {
