fb529c4526e13040cd3eeb810d62604ad3809395
Ben-Boyte <zissette1@gmail.com>
2022-05-16 12:11:34 -0500

    removed schedule function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,14 @@
 var date = $("#currentDay");
 var currentTime = moment().hour()
 date.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-container.append(date);
 
-function scheduler() {
-    for (i = 0; i < time.length; i++) {
-    let timeInteger = parseInt(time[i])
 
-        if (timeInteger < currentTime) {
-            var moment = "past"
-        }
-       else if (timeInteger > currentTime) {
-            var moment = "future"
-        }
-       else if (timeInteger == currentTime) {
-            var moment = "present"
-        }
-    }
-    
-    
+function store() {
+    localStorage.setItem($(this).attr("id"), $(this).prev().val());
+    $(this).prev().transfer({
+        to: $( $(this) ),
+        duration: 5000
+    })
 }
 
-window.onload = scheduler();
+$(".saveBtn").on("click", store)
