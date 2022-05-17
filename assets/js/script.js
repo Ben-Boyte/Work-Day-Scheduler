a43a298a935ce2d207e6be93ec890fd5599e3e3d
Ben-Boyte <zissette1@gmail.com>
2022-05-17 14:21:22 -0500

    updated momentjs date and minor css fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,11 @@ var time = ["9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00
 var regularTime = ["9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm"]
 var date = $("#currentDay");
 var currentTime = moment().hour()
-date.text(moment().format('MMMM Do YYYY, h:mm a'));
+date.text(moment().format("MMM Do YYYY"));
 
 function scheduler() {
     for (i = 0; i < time.length; i++) {
+        
     var timeInteger = parseInt(time[i])
         if (timeInteger < currentTime) {
             var placeInTime = "past"
@@ -17,6 +18,7 @@ function scheduler() {
        else if (timeInteger == currentTime) {
             var placeInTime = "present"
         }
+
     var row = $("<div>");
             row.attr("class", "row");
             container.append(row);
